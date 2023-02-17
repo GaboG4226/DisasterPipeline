a0600ce039562062dd88e5202a80a7ae5cd72eca
GaboG4226 <gabogarcia4226@outlook.com>
2023-02-17 13:27:36 -0600

    ETL Pipeline Preparation and process_data.py finished

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,104 @@
+# import libraries
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    '''
+    returns a merged dataframe given two different datasets
+    
+    INPUT: 
+        messages_filepath: path to read messages csv file
+        categories_filepath: path to read categories csv file
+    
+    OUTPUT:
+        merged dataframe based on "id" column
+    '''
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    
+    '''
+    returns a cleaned dataframe where the next steps are followed: 
+    
+    1. Creation of a dataframe with 36 columns of all the different categories
+    2. Obtention of the categories of the dataset and using them as new column names
+    3. Change the values from text to binary numbers in each of the new 36 columns
+    4. Duplicate dropping
+    
+    INPUT: 
+        df: dataframe to be cleaned
+    
+    OUTPUT:
+        cleaned dataframe
+    '''
+    
+    
+    # Creation of a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(pat=';', expand=True)
+    
+    # First row of the categories dataframe to get the different values
+    row = pd.Series(df.categories[0])
 
+    # Separation of category names with split and apply functions
+    category_colnames = row.str.split(pat=';', expand=True).apply(lambda value: value.str.slice(0,-2))
+
+    # Dataframe to list
+    category_colnames = category_colnames.values.tolist()
+
+    # Flatten of list
+    category_colnames = [item for elem in category_colnames for item in elem]
+    
+    # Rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    
+    # Loop to change columns to numerical values
+    for column in categories:
+    
+    # Set each value to be the last character of the string
+    categories[column] = categories[column].str[-1:]
+    
+    # Convertion of column from string to numeric
+    categories[column] = pd.to_numeric(categories[column])
+    
+    # Dropping the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+    
+    # Concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # Drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
+        
 
 def save_data(df, database_filename):
-    pass  
+    
+    '''
+    save dataframe to a sqlite database
+ 
+    
+    INPUT: 
+        df: dataframe to be saved
+        database_filename: name of the database where the dataframe will be stored
+    
+    OUTPUT:
+        cleaned dataframe
+    '''
+    
+    engine = create_engine(database_filename)
+    df.to_sql('messages_categories', engine, index=False) 
 
 
 def main():
