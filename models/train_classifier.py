c82d1f0f7a4cf433d8f87fb25397c6d26a0dc791
GaboG4226 <gabogarcia4226@outlook.com>
2023-02-24 14:24:01 -0600

    Created train_classifier.py
    It was created a train_classifier.py with the steps of the ML Pipeline, but in a .py file

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,157 @@
 import sys
 
+# General libraries
+import pandas as pd
+import lazypredict
+import numpy as np
+import re
+
+# Database libraries
+from sqlalchemy import create_engine
+
+# Tokenization libraries
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+
+# ML libraries
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier, MultiOutputRegressor
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+# Best model for our data analysis
+from lazypredict.Supervised import LazyClassifier, LazyRegressor
+
 
 def load_data(database_filepath):
-    pass
+    '''
+    loads the data from a database 
+    
+    INPUT: 
+        database_filepath: filepath in which the database is located
+    
+    OUTPUT:
+        dataframe with the information read on the database
+    
+    '''
+    
+    # create connection to the sqlite database
+    engine = create_engine('sqlite:///disaster_pipeline.db')
+    
+    # creation of dataframe with specific table from the database
+    df = pd.read_sql('messages_categories', engine)
+    
+    return df
 
 
 def tokenize(text):
-    pass
+    
+    '''
+    tokenize texts by previously lemmatizing, eliminating stop words and changing words to lower case
+    
+    INPUT:
+        text: texts to be tokenized
+        
+    OUTPUT:
+        text tokenized with all the intermediate steps performed
+    '''
+    
+    # Definition of lemmatizer to be used for tokenization
+    lemmatizer = WordNetLemmatizer()
+    
+    # Stop words definition
+    stop_words = stopwords.words("english")
+    
+    # Normalization case and punctuation removal
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # Tokenization of words
+    tokens = word_tokenize(text)
+    
+    # Stop words removal
+    tokens = [lemmatizer.lemmatize(w) for w in tokens if w not in stop_words]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    
+    '''
+    builds the model that is going to be used through a pipeline, where gridsearch is used for tunning hyperparameters
+        
+        
+    OUTPUT:
+        pipeline used as model for the training
+    '''
+    
+    # pipeline definition
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(LogisticRegression(max_iter=2000))),
+    ])
+    
+    # parameters to be used for gridsearch
+    parameters = {
+        'clf__estimator__C': [0.1, 1, 10]
+    }
+    
+    # grid search for tunning hyperparameters
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    '''
+    evaluates the model by making a predictions with the X_test
+    
+    INPUT:
+        model: model to be evaluated with the test set
+        X_test: input test set that will be used to generate predictions with the model
+        Y_test: results test set that will be used to evaluate the model performance
+        category_names: categories to be analyzed within each column of the Y_test set
+        
+    OUTPUT:
+        print the classification report by column name with the categories specified in the input
+    '''
+    # transform to dataframe to get classification_report by column
+    predicted_df = pd.DataFrame(model.predict(X_test), columns=Y_test.columns)
+
+    # loop to get report by column
+    for column in Y_test.columns:
+        
+        # creation and printing of report
+        report = classification_report(y_test[column], predicted_df[column], target_names=category_names, zero_division=0)
+        print("\n Classification [Original] report for column '{}': \n {}".format(column, report))
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    '''
+    saves the model to a pickle file
+    
+    INPUT:
+        model: model to be saved in pickle file
+        model_filepath: path where we want our model to be saved
+        
+    OUTPUT:
+        model saved in a .pkl file in the specified filepath
+    '''
+    
+    pickle.dump(pipeline, open('pipeline.pkl', 'wb'))
 
 
 def main():
